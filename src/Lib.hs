3a63f101998e872209121545cf0e3a20da22f9c4
Francososa <franky.sosa@gmail.com>
2018-09-09 15:34:22 -0600

    Remove ByteString test code. Define functions to make Int into Byte

--- src/Lib.hs
+++ src/Lib.hs
@@ -6,16 +6,12 @@ module Lib
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Char8 as BC
 
-sampleBytes :: BS.ByteString
-sampleBytes = "Hello!"
-
-sampleString :: String
-sampleString = BC.unpack sampleBytes
-
-bcInt :: BC.ByteString
-bcInt = "6"
-
-bcToInt :: BC.ByteString -> Int
-bcToInt = read . BC.unpack
 
 someFunc = putStrLn "Hello"
+
+intToChar :: Int -> Char
+intToChar int = toEnum safeInt
+  where safeInt = int `mod` 255
+
+intToBc :: Int -> BC.ByteString
+intToBc int = BC.pack [intToChar int]
