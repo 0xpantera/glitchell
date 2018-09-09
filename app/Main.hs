0e159535b7f06a857ebd1de0ca00a1afe46c823c
Francososa <franky.sosa@gmail.com>
2018-09-09 17:01:26 -0600

    Define lambda to chain together IO actions with foldM. Add glitch result.

--- app/Main.hs
+++ app/Main.hs
@@ -4,6 +4,15 @@ import Lib
 import System.Environment
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Char8 as BC
+import Control.Monad
+
+
+glitchActions :: [BC.ByteString -> IO BC.ByteString]
+glitchActions = [randomReplaceByte
+                ,randomSortSection
+                ,randomReplaceByte
+                ,randomSortSection
+                ,randomReplaceByte]
 
 
 main :: IO ()
@@ -11,7 +20,7 @@ main = do
   args <- getArgs
   let fileName = head args
   imageFile <- BC.readFile fileName
-  glitched <- randomSortSection imageFile
+  glitched <- foldM (\bytes func -> func bytes) imageFile glitchActions
   let glitchedFileName =  mconcat ["glitched_", fileName]
   BC.writeFile glitchedFileName glitched
   print "done"
