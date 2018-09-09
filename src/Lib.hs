a75475e5c229c1e66f1bec64fca171fb114ed41d
Francososa <franky.sosa@gmail.com>
2018-09-09 15:43:56 -0600

    Implement function to replace bytes in file

--- src/Lib.hs
+++ src/Lib.hs
@@ -13,5 +13,15 @@ intToChar :: Int -> Char
 intToChar int = toEnum safeInt
   where safeInt = int `mod` 255
 
+
 intToBc :: Int -> BC.ByteString
 intToBc int = BC.pack [intToChar int]
+
+
+replaceByte :: Int -> Int -> BC.ByteString -> BC.ByteString
+replaceByte loc charVal bytes = mconcat [before,newChar,after]
+  where (before, rest) = BC.splitAt loc bytes
+        after = BC.drop 1 rest
+        newChar = intToBC charVal
+
+
