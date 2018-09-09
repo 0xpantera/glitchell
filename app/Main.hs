b43767d0a3198e2561e59b3cb2d3fc430918ba5f
Francososa <franky.sosa@gmail.com>
2018-09-09 15:27:11 -0600

    Define program layout in main

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,17 @@
 module Main where
 
 import Lib
+import System.Environment
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as BC
+
 
 main :: IO ()
-main = someFunc
+main = do
+  args <- getArgs
+  let fileName = head args
+  imageFile <- BC.readFile fileName
+  glitched <- imageFile
+  let glitchedFileName <- mconcat ["glitched_", fileName]
+  BC.writeFile glitchedFileName glitched
+  print "done"
